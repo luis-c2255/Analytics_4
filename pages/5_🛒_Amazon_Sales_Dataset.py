9dc2359b20e03c99b2d3c2a1b63433c758e1f756
luis-c2255 <lmodesto63@gmail.com>
2026-03-29 04:34:53 +0200

    Update title to include shopping cart emoji

--- pages/5_🛒_Amazon_Sales_Dataset.py
+++ pages/5_🛒_Amazon_Sales_Dataset.py
@@ -6,4 +6,4 @@ st.set_page_config(
     initial_sidebar_state="expanded"
 )
 
-st.title(":gray[Amazon Sales Dataset Analysis]", text_alignment="center")
+st.title("🛒 :gray[Amazon Sales Dataset Analysis]", text_alignment="center")
