e701395736c2f30581012b263fefb45fd7292595
luis-c2255 <lmodesto63@gmail.com>
2026-03-29 04:33:33 +0200

    Add Streamlit app for global suicide rates analysis

---
+++ pages/6_☠️_Global_Suicide_Rates_per_Country.py
@@ -0,0 +1,9 @@
+import streamlit as st
+from utils.theme import Components
+
+st.set_page_config(
+    layout="wide",
+    initial_sidebar_state="expanded"
+)
+
+st.title(":red[Global Suicide Rates per Country 2000-2021 Analysis]", text_alignment="center")
