fb45b282df23c717ee5f8cc5211e78b91fcfd82f
luis-c2255 <lmodesto63@gmail.com>
2026-03-29 04:34:36 +0200

    Update title to include emoji in the header

--- pages/6_☠️_Global_Suicide_Rates_per_Country.py
+++ pages/6_☠️_Global_Suicide_Rates_per_Country.py
@@ -6,4 +6,4 @@ st.set_page_config(
     initial_sidebar_state="expanded"
 )
 
-st.title(":red[Global Suicide Rates per Country 2000-2021 Analysis]", text_alignment="center")
+st.title("☠️ :red[Global Suicide Rates per Country 2000-2021 Analysis]", text_alignment="center")
