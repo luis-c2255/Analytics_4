a3431245b48149e509c42d5389ed987a72025b6c
luis-c2255 <lmodesto63@gmail.com>
2026-03-29 04:32:50 +0200

    Add Amazon Sales Dataset analysis page

---
+++ pages/5_🛒_Amazon_Sales_Dataset.py
@@ -0,0 +1,9 @@
+import streamlit as st
+from utils.theme import Components
+
+st.set_page_config(
+    layout="wide",
+    initial_sidebar_state="expanded"
+)
+
+st.title(":gray[Amazon Sales Dataset Analysis]", text_alignment="center")
