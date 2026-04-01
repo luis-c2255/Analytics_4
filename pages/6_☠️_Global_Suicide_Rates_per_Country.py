42149975ba603e0875333455629a01aba4441399
luis-csm86 <lmodesto63@gmail.com>
2026-04-01 09:07:21 +0200

    add

--- pages/6_☠️_Global_Suicide_Rates_per_Country.py
+++ pages/6_☠️_Global_Suicide_Rates_per_Country.py
@@ -298,7 +298,7 @@ st.subheader("🔝 :green[Country Rankings]", divider="green")
 st.markdown("   ")
 col1, col2 = st.columns(2)
 with col1:
-    st.markdown("🔴##### :green[Highest Suicide Rates]")
+    st.markdown("🔴 :green[Highest Suicide Rates]")
     top_20 = df_filtered[df_filtered['age_group'] == 'ALL'].groupby('country')['suicide_rate'].mean().sort_values(ascending=False).head(20).reset_index()
     top_20.index = range(1, len(top_20) + 1)
     top_20.columns = ['Country', 'Rate per 100k']
@@ -307,7 +307,7 @@ with col1:
     st.dataframe(top_20, width="stretch", height=600)
 
 with col2:
-    st.markdown("🟢##### :green[Lowest Suicide Rates]")
+    st.markdown("🟢 :green[Lowest Suicide Rates]")
     bottom_20 = df_filtered[df_filtered['age_group'] == 'ALL'].groupby('country')['suicide_rate'].mean().sort_values(ascending=True).head(20).reset_index()
     bottom_20.index = range(1, len(bottom_20) + 1)
     bottom_20.columns = ['Country', 'Rate per 100k']
